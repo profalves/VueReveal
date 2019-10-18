6ee7895367eda9e968aad50af33768d5215915c0
Rodrigo Alves <igo.sud@hotmail.com>
2019-10-17 22:33:53 -0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# VueReveal
-Created with CodeSandbox
---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <div class="reveal">
+      <div class="slides">
+        <section>Single Horizontal Slide</section>
+        <section>
+          <section>Vertical Slide 1</section>
+          <section>Vertical Slide 2</section>
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+import Reveal from 'reveal.js/js/reveal'
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  },
+  mounted() {
+    Reveal.initialize()
+  }
+};
+</script>
+
+<style>
+@import url('../node_modules/reveal.js/css/reveal.css');
+@import url('../node_modules/reveal.js/css/theme/white.css');
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+  height: 100vh;
+}
+
+</style>
